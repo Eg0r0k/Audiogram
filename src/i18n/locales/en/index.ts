73891afb6bb470293e59e39880755d19cbb0b233
Eg0r0k <egokakill@gmail.com>
2026-01-03 13:46:51 +0300

    feat: add new translations in nav

--- src/i18n/locales/en/index.ts
+++ src/i18n/locales/en/index.ts
@@ -3,10 +3,12 @@ import player from "./player.json";
 import track from "./track.json";
 import media from "./media.json";
 import settings from "./settings.json";
+import nav from "./nav.json";
 export default {
   common,
   player,
   track,
   media,
   settings,
+  nav,
 };
