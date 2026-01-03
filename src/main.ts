fdced06a681c4cb7b73859998009380c7cbe3a35
Eg0r0k <egokakill@gmail.com>
2026-01-03 20:03:31 +0300

    feat: add pinia persist lib

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import "./style.css";
 import { createPinia } from "pinia";
 import { i18n } from "./i18n";
 import vRipple from "./directives/ripple";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
