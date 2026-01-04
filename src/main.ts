0c9d8b5d0b00cee074d4afd17aab85d7ee82ca1c
Eg0r0k <egokakill@gmail.com>
2026-01-04 23:10:45 +0300

    feat add ofetch and tanstack query libs

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,32 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { VueQueryPlugin } from "@tanstack/vue-query";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import App from "./App.vue";
 import router from "./router";
-import "./style.css";
-import { createPinia } from "pinia";
 import { i18n } from "./i18n";
 import vRipple from "./directives/ripple";
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import "./style.css";
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
+
 const app = createApp(App);
+
 app.use(router);
 app.use(pinia);
 app.use(i18n);
+app.use(VueQueryPlugin, {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5, // 5 min
+        retry: 2,
+        refetchOnWindowFocus: false,
+      },
+    },
+  },
+});
+
 app.directive("ripple", vRipple);
 app.mount("#app");
