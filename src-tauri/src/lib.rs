0003110ddd7abefa576c41e3c84338870f2c4de0
Eg0r0k <egokakill@gmail.com>
2026-01-04 19:29:34 +0300

    chore(tauri): setup tauri to using FS

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,20 +8,21 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
             let files: Vec<String> = std::env::args().skip(1).collect();
-            
+
             if !files.is_empty() {
                 let app_handle = app.handle().clone();
-                
+
                 std::thread::spawn(move || {
                     std::thread::sleep(std::time::Duration::from_millis(500));
                     let _ = app_handle.emit("files-opened", files);
                 });
             }
-            
+
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![greet])
