bf3169d14daa775860c0d73b688e7a862f984551
Eg0r0k <egokakill@gmail.com>
2026-01-07 20:29:08 +0300

    refactor: delete usless file tabSync

--- src/components/player/PlayerBarNotification.vue
+++ src/components/player/PlayerBarNotification.vue
@@ -1,19 +1,16 @@
 <script setup lang="ts">
 import { Volume2 } from "lucide-vue-next";
-import { isPlayingElsewhere, playingTab } from "@/composables/tabSync";
 </script>
 
 <template>
   <div
-    v-if="isPlayingElsewhere && playingTab"
+    v-if="false "
     class="flex bg-primary flex-row text-primary-foreground justify-center items-center h-8 px-3 rounded-sm gap-2"
   >
     <Volume2 :size="16" />
     <span class="text-sm font-semibold">
       {{ $t("player.herePlaying") }}:
-      <span class="ml-1">
-        {{ playingTab.browser }} {{ playingTab.browser.name }}
-      </span>
+      <span class="ml-1" />
     </span>
   </div>
 </template>
