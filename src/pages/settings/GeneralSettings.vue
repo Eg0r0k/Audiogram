681a72826d8d4c664bbf09db74c827bea9ef2fbb
Eg0r0k <egokakill@gmail.com>
2026-02-27 21:21:35 +0300

    refactor: make base for general settings

--- src/pages/settings/GeneralSettings.vue
+++ src/pages/settings/GeneralSettings.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+import Scrollable from "@/components/ui/scrollable/Scrollable.vue";
+import SettingsGroup from "@/modules/settings/components/SettingsGroup.vue";
+import SettingsHeader from "@/modules/settings/components/SettingsHeader.vue";
+
+</script>
+
 <template>
-  <div>
-    General
-  </div>
+  <Scrollable
+    direction="vertical"
+    class="flex-1"
+  >
+    <div class="pb-8">
+      <SettingsHeader :title="$t('settings.index.general')" />
+      <SettingsGroup>
+        <div class="px-4 py-3">
+          <div class="text-primary font-medium mb-1">
+            General settings
+          </div>
+        </div>
+      </SettingsGroup>
+    </div>
+  </Scrollable>
 </template>
