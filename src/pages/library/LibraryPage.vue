5b749e52c03949709545cf96c6eea9479d173727
Eg0r0k <egokakill@gmail.com>
2026-02-27 21:17:44 +0300

    refactor: start design library page

--- src/pages/library/LibraryPage.vue
+++ src/pages/library/LibraryPage.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>
-    lib
-  </div>
+  <Scrollable class="flex-1">
+    <div class="flex w-full items-center justify-between px-4 pt-4">
+      <span class=" text-lg font-medium">
+        {{ $t('library.title') }}
+      </span>
+      <div class="flex gap-1">
+        <Button
+          variant="ghost"
+          class=" rounded-full"
+          size="icon"
+        >
+          <IconSearch class="size-5" />
+        </Button>
+        <Button
+          variant="ghost"
+          class=" rounded-full"
+          size="icon"
+        >
+          <IconPlus class="size-5" />
+        </Button>
+      </div>
+    </div>
+    <div class="px-4 ">
+      adsas
+    </div>
+  </Scrollable>
 </template>
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import Scrollable from "@/components/ui/scrollable/Scrollable.vue";
+import IconPlus from "~icons/tabler/plus";
+import IconSearch from "~icons/tabler/search";
+
+</script>
